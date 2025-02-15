0c59e8c03e9aead749693b951f68eda0a816983c
abdul <up2dul@gmail.com>
2025-02-16 02:16:43 +0700

    feat(comps/layout): topbar contents

--- src/components/layout/topbar.tsx
+++ src/components/layout/topbar.tsx
@@ -1,4 +1,16 @@
-import { Box } from "@mui/material";
+import {
+  ChatOutlined,
+  ExpandMore,
+  NotificationsOutlined,
+} from "@mui/icons-material";
+import {
+  Avatar,
+  Badge,
+  Box,
+  IconButton,
+  Switch,
+  Typography,
+} from "@mui/material";
 
 export const Topbar = () => {
   return (
@@ -12,7 +24,79 @@ export const Topbar = () => {
         top: 0,
         left: 0,
         zIndex: 1,
+        paddingX: "30px",
+        display: "flex",
+        justifyContent: "flex-end",
+        alignItems: "center",
+        gap: "28px",
       }}
-    />
+    >
+      <Switch />
+
+      <Box
+        sx={{
+          display: {
+            xs: "none",
+            md: "flex",
+          },
+          alignItems: "center",
+          gap: "15px",
+        }}
+      >
+        <IconButton
+          aria-label="notifications"
+          sx={{
+            bgcolor: "#EFF4FB",
+            color: "#64748B",
+            border: "1px solid #E2E8F0",
+          }}
+        >
+          <NotificationsOutlined />
+        </IconButton>
+        <IconButton
+          aria-label="chat"
+          sx={{
+            bgcolor: "#EFF4FB",
+            color: "#64748B",
+            border: "1px solid #E2E8F0",
+          }}
+        >
+          <Badge color="error" variant="dot">
+            <ChatOutlined />
+          </Badge>
+        </IconButton>
+      </Box>
+
+      <Box sx={{ display: "flex", gap: "15px" }}>
+        <Box
+          sx={{
+            textAlign: "right",
+            display: {
+              xs: "none",
+              md: "block",
+            },
+          }}
+        >
+          <Typography
+            variant="body2"
+            sx={{ color: "#212B36", fontWeight: 600 }}
+          >
+            John Doe
+          </Typography>
+          <Typography
+            variant="caption"
+            sx={{ color: "#637381", fontWeight: 600 }}
+          >
+            Verified Member
+          </Typography>
+        </Box>
+        <Box sx={{ display: "flex", gap: "10px" }}>
+          <Avatar alt="John Doe" src="" />
+          <IconButton aria-label="settings">
+            <ExpandMore />
+          </IconButton>
+        </Box>
+      </Box>
+    </Box>
   );
 };
